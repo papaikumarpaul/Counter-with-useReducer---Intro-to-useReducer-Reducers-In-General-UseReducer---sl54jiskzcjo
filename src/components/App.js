fc8557a6a3322fa50c65ebf8032f892fa3c6fd3b
papai <Papai Pc@Lenovo.com>
2023-04-08 16:44:24 +0530

    code

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React, { useReducer } from 'react'
-import { counterReducer } from '../reducers/counterReducer';
-import '../styles/App.css';
+import React, { useReducer } from "react";
+import { counterReducer } from "../reducers/counterReducer";
+import "../styles/App.css";
 const App = () => {
-const [state,dispatch] = useReducer(counterReducer,{/*initial state to be placed here*/})
+  const [state, dispatch] = useReducer(counterReducer, { count: 0 });
   return (
     <div id="main">
-
+      <button
+        id="increment-btn"
+        onClick={() => dispatch({ type: "increment" })}
+      >
+        +
+      </button>
+      <span id="count">{state.count}</span>
+      <button
+        id="decrement-btn"
+        onClick={() => dispatch({ type: "decrement" })}
+      >
+        -
+      </button>
     </div>
-  )
-}
-
+  );
+};
 
 export default App;
--- src/reducers/counterReducer.js
+++ src/reducers/counterReducer.js
@@ -1,5 +1,12 @@
-const counterReducer = (state,action) =>{
+const counterReducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      return state;
+  }
+};
 
-}
-
-export {counterReducer}
+export { counterReducer };
